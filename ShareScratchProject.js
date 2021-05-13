d5b50f4ac8b730a801a34cc91f55c6d284c781a0
ShluffyMonster <81823039+Shluffy@users.noreply.github.com>
2021-05-13 14:49:52 -0400

    Delete ShareScratchProject.js

--- ShareScratchProject.js
+++
@@ -1,6 +0,0 @@
-//This only works on the main project page, not in the editor.
-//Copy "javascript:document.getElementsByClassName("banner-button")[0].click();" for bookmarklets
-//or "document.getElementsByClassName("banner-button")[0].click();" on console to use
-//Credit to shluffy for this bookmarklet
-
-javascript:document.getElementsByClassName("banner-button")[0].click();
