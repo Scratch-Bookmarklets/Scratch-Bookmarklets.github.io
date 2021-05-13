b485f75c27bb6e9f651b6117e91ec2fdf7c933c1
ShluffyMonster <81823039+Shluffy@users.noreply.github.com>
2021-05-13 14:49:50 -0400

    Delete RedirectToScratchUser.js

--- RedirectToScratchUser.js
+++
@@ -1,6 +0,0 @@
-//This bookmarklet will redirect you to a scratch user.
-//Copy "javascript:var user = prompt("What User Do You want to go to?"); window.location.href="https://scratch.mit.edu/users/"+user+"/";" for bookmarklets
-//or "var user = prompt("What User Do You want to go to?"); window.location.href="https://scratch.mit.edu/users/"+user+"/";" on console to use.
-//Credit to shluffy for this bookmarklet
-
-javascript:var user = prompt("What User Do You want to go to?"); window.location.href="https://scratch.mit.edu/users/"+user+"/";
