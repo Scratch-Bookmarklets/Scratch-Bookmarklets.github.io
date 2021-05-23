9df051f582ec0574c9e5fcfd91bac89ca96da1ed
FunctionalMetatable <67440879+FunctionalMetatable@users.noreply.github.com>
2021-05-23 20:03:21 +0100

    Update bookmarklet.js

--- bookmarklets/view-post/bookmarklet.js
+++ bookmarklets/view-post/bookmarklet.js
@@ -1,3 +1,6 @@
+window.copy_paste=function(e){let t=e.substring(1),s=document.querySelector(`#p${t}`).querySelector(".username").innerText;fetch(`/discuss/post/${t}/source`).then(e=>e.text()).then(e=>{paste(`[quote=${s}]\n[url=https://scratch.mit.edu/discuss/post/${t}][color=grey][[]View Post][/color][/url]\n${e}[/quote]`)})};
+// The below, unminified code is in comments as theres a strange bug that breaks this bookmarklet.
+/*
 window.copy_paste = function(qs) {
   let id = qs.substring(1)
   let post = document.querySelector(`#p${id}`)
@@ -7,3 +10,4 @@ window.copy_paste = function(qs) {
     paste(`[quote=${username}]\n[url=https://scratch.mit.edu/discuss/post/${id}][color=grey][[]View Post][/color][/url]\n${source}[/quote]`)
   })
 }
+*/
