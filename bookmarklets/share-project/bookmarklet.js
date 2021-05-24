f4c88e9871109cd26b28bd444958f22202eaf55d
ShluffyMonster <81823039+Shluffy@users.noreply.github.com>
2021-05-24 15:07:36 -0400

    Update bookmarklet.js

--- bookmarklets/share-project/bookmarklet.js
+++ bookmarklets/share-project/bookmarklet.js
@@ -1,1 +1,6 @@
+if (parser.hostname === scratch.mit.edu) {
 document.getElementsByClassName("banner-button")[0].click();
+}
+else {
+alert("Please click the bookmarklet on the scratch website (scratch.mit.edu)")
+}
