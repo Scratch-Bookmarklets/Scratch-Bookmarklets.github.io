7f63ebfdde66f0d1f110a5d5117d966553d32263
ShluffyMonster <81823039+Shluffy@users.noreply.github.com>
2021-05-24 15:07:21 -0400

    Update bookmarklet.js

--- bookmarklets/open-turbowarp/bookmarklet.js
+++ bookmarklets/open-turbowarp/bookmarklet.js
@@ -1,1 +1,6 @@
+if (parser.hostname === scratch.mit.edu) {
 window.location.href = `https://turbowarp.org/${window.location.href.substring(33)}`
+}
+else {
+alert("Please click the bookmarklet on the scratch website (scratch.mit.edu)")
+}
