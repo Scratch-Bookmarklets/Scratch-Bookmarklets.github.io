a8051f004f7d89cd7b453acf86bb7fba626422bb
ShluffyMonster <81823039+Shluffy@users.noreply.github.com>
2021-05-24 15:06:42 -0400

    Update bookmarklet.js

--- bookmarklets/goto-user/bookmarklet.js
+++ bookmarklets/goto-user/bookmarklet.js
@@ -1,2 +1,7 @@
-let user = prompt("Which user would you like to go to?");
+if (parser.hostname === scratch.mit.edu) {
+  let user = prompt("Which user would you like to go to?");
 window.location.href = `https://scratch.mit.edu/users/${user}/`;
+}
+else {
+alert("Please click the bookmarklet on the scratch website (scratch.mit.edu)")
+}
