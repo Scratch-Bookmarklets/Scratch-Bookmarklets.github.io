61b5ab4da91040e981ee0a64fb4ddab607b0830d
ShluffyMonster <81823039+Shluffy@users.noreply.github.com>
2021-05-24 15:03:06 -0400

    Update bookmarklet.js

--- bookmarklets/auto-bump-topic/bookmarklet.js
+++ bookmarklets/auto-bump-topic/bookmarklet.js
@@ -1,2 +1,7 @@
+if (parser.hostname === scratch.mit.edu) {
 document.querySelector(".markup").value = "Bump";
 document.getElementsByClassName("button grey")[2].click();
+}
+else {
+alert("Please click the bookmarklet on the scratch website (scratch.mit.edu)")
+}
