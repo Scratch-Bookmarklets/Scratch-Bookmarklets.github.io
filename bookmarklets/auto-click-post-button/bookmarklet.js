4342ad236cffe162c3aa100ee50515e90c2f10ff
ShluffyMonster <81823039+Shluffy@users.noreply.github.com>
2021-05-24 15:05:30 -0400

    Update bookmarklet.js

--- bookmarklets/auto-click-post-button/bookmarklet.js
+++ bookmarklets/auto-click-post-button/bookmarklet.js
@@ -1,1 +1,6 @@
-document.getElementsByClassName("button grey")[2].click();
+if (parser.hostname === scratch.mit.edu) {
+  document.getElementsByClassName("button grey")[2].click();
+  }
+else {
+alert("Please click the bookmarklet on the scratch website (scratch.mit.edu)")
+}
