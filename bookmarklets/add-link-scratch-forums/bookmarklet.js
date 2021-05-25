fe75083755f861cb55fbb466a654c81f2ab11bbd
ShluffyMonster <81823039+Shluffy@users.noreply.github.com>
2021-05-25 11:57:16 -0400

    Update bookmarklet.js

--- bookmarklets/add-link-scratch-forums/bookmarklet.js
+++ bookmarklets/add-link-scratch-forums/bookmarklet.js
@@ -1,1 +1,6 @@
-let url = prompt("Url"); let url_text = prompt("Text to link")
+//Broken - will turn the whole page into the BBCode. Will be fixed before being added
+
+let url = prompt("Url"); 
+let url_text = prompt("Text to link"); 
+let textarea = document.getElementsByClassName("markup"); 
+textarea[0].value = `[url=${url}]${url_text}[/url]`;
