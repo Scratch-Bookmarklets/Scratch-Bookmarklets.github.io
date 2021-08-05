5f713fc993b18510d09e5aec6369e651a327a7f1
Zack <xitzdiamondx@protonmail.ch>
2021-08-05 13:43:27 -0500

    Update bookmarklet.js
    b5c833f43cdd0cdd96136699d6911908b04eb8eb

--- bookmarklets/open-forkphorus/bookmarklet.js
+++ bookmarklets/open-forkphorus/bookmarklet.js
@@ -1,5 +1,3 @@
 window.location.href = `https://forkphorus.github.io//${window.location.href.substring(
   33
 )}`;
-=======
-window.location.href = `https://forkphorus.github.io//${window.location.href.substring(33)}`
